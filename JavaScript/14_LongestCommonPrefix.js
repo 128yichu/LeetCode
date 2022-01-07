ccdce8d6678ad4802f43ab96c40777b8d6c813e5
128yichu <71680224+128yichu@users.noreply.github.com>
2022-01-07 16:24:42 +0800

    update

---
+++ JavaScript/14_LongestCommonPrefix.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+ var longestCommonPrefix = function(strs) {
+    if(strs == null || strs.length == 0) return "";
+
+    var same = strs[0];
+
+    for(var i = 1 ; i<strs.length ; i++){
+        var str = strs[i];
+
+        var j = 0;
+        for(; j < same.length ; j++){
+            if(same[j] != str.charAt(j)){
+                break;
+            } 
+        }
+        same = same.slice(0,j);
+    }
+
+    return same;
+};
